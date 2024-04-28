7e878424af28c046209420c7957d5b273d5e36a9
SouadR0 <131975482+SouadR0@users.noreply.github.com>
2024-04-28 17:41:28 +0300

    Update Department.java

--- stage-2/Company-system/src/main/java/com/mycompany/company/system/Department.java
+++ stage-2/Company-system/src/main/java/com/mycompany/company/system/Department.java
@@ -8,3 +8,17 @@ public String name;
 public Address address;
 public Employee head;
 ArrayList<Office> offices;
+public Department(String name, Address address, Employee head){
+ this.name = name;
+ this.address = address;
+ this.head = head;
+ this.offices = new ArrayList<>();
+}
+   public void addOffice(Office office){
+       this.offices.add(office);
+   }
+    public void removeOffice(Office office){
+        this.offices.remove(office);
+    }
+}
+    
